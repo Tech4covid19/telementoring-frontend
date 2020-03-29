908b084de0c0b14b5374006561269c70349f3d82
Nuno Lopes Fernandes <nuno@nlfsoftware.com>
2020-03-29 02:33:43 +0100

    registration form with email-password
    user account is creater on firebase

--- src/app/components/login/registration/registration.component.ts
+++ src/app/components/login/registration/registration.component.ts
@@ -1,4 +1,12 @@
+import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { MustMatch } from 'src/app/helpers/must-match.validator';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6)
+        ]),
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      }
+    );
   }
 
+  onSubmit() {
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.authService.register(
+      this.registerForm.value.email,
+      this.registerForm.value.password
+    );
+  }
 }
