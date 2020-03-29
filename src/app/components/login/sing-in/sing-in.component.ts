c52a9fdd715255aeaaf0403cce513a60cf3ee1cd
Nuno Lopes Fernandes <nuno@nlfsoftware.com>
2020-03-29 03:00:52 +0100

    login form
    login using firebase

--- src/app/components/login/sing-in/sing-in.component.ts
+++ src/app/components/login/sing-in/sing-in.component.ts
@@ -1,5 +1,11 @@
 import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sing-in',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sing-in.component.scss']
 })
 export class SingInComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  loginForm: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  handleSingIn() {}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  onSubmit() {
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.authService.login(
+      this.loginForm.value.email,
+      this.loginForm.value.password
+    );
+  }
 }
