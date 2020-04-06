32ac7525aaadec25a90b9915288609f89df9f54d
gitlab <gitlab@netsoft.eti.br>
2020-04-06 08:18:49 -0300

    inclusão do snackbar no componente medical-profile

--- src/app/components/medical-profile/medical-profile.component.ts
+++ src/app/components/medical-profile/medical-profile.component.ts
@@ -5,7 +5,9 @@ import {
   FormGroup,
   Validators
 } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MustMatch } from 'src/app/helpers/must-match.validator';
+import { SnackBarComponent } from '../snack-bar/snack-bar.component';
 
 @Component({
   selector: 'app-medical-profile',
@@ -14,13 +16,19 @@ import { MustMatch } from 'src/app/helpers/must-match.validator';
 })
 export class MedicalProfileComponent implements OnInit {
   medicalProfileForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+  durationInSeconds = 5;
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.createGroupForm();
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.openSnackBar();
+  }
 
   private createGroupForm() {
     this.medicalProfileForm = this.formBuilder.group(
@@ -45,5 +53,11 @@ export class MedicalProfileComponent implements OnInit {
         validator: MustMatch('password', 'confirmPassword'),
       }
     );
+  }
+
+  private openSnackBar() {
+    this.snackBar.openFromComponent(SnackBarComponent, {
+      duration: this.durationInSeconds * 1000,
+    });
   }
 }
