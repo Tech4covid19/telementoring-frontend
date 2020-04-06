08a4481d07f84eb7425f2163c0b5c4a562fba45e
gitlab <guilherme@netsoft.eti.br>
2020-04-06 11:22:43 -0300

    inclusion of the Medical Profile model

---
+++ src/app/domain/medical.profile.ts
@@ -0,0 +1,14 @@
+export type MedicalProfile = {
+  email: string;
+  sex: string;
+  name: string;
+  country: string;
+  surname: string;
+  hospital: string;
+  telephone: string;
+  cellPhone: string;
+  password: string;
+  speciality: string;
+  confirmPassword: string;
+  birth: string;
+};
