794a27bdd98da8e60ab9fb9e8b1c7febac3ee676
gitlab <guilherme@netsoft.eti.br>
2020-04-07 10:58:29 -0300

    Settings for the component to request a new password, search for and validate the information entered by the user on the server

--- src/app/components/login/reset-password/reset-password.component.ts
+++ src/app/components/login/reset-password/reset-password.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { SnackbarComponent } from '../../snack-bar/snack-bar.component';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +15,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
-
-  constructor() { }
+  resetForm: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private snackbar: SnackbarComponent,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.createGroupForm();
   }
 
+  onSubmit() {
+    // stop here if form is invalid
+    const email = this.resetForm.get("email");
+    if (this.resetForm.invalid) {
+      if (email.errors != null) {
+        this.snackbar.openSnackBar(
+          'Invalid or blank email !',
+          'error'
+        );
+        return;
+      }
+    }
+
+    this.authService.sendPasswordResetEmail(email.value)
+      .then(
+        data => {
+          this.snackbar.openSnackBar(
+            'The information was sent to your email. Please check !',
+            'success'
+          );
+          this.router.navigate(['/login']);
+        }
+      ).catch(
+        error => {
+          console.log(error);
+          this.snackbar.openSnackBar(
+            error.message,
+            'error'
+          );
+        }
+      );
+  }
+
+  private createGroupForm() {
+    this.resetForm = this.formBuilder.group(
+      {
+        email: new FormControl('', [Validators.required, Validators.email])
+      }
+    );
+  }
 }
