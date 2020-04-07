315afec794a7f3183e54c3c7caad31fc58f06518
gitlab <guilherme@netsoft.eti.br>
2020-04-07 13:29:06 -0300

    Password policy adjustments

--- src/app/helpers/password.validator.ts
+++ src/app/helpers/password.validator.ts
@@ -1,24 +1,28 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
 
-export function PasswordValidator(): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } | null => {
+export class PasswordValidator {
+
+  static Validator(controle: AbstractControl) {
+    const password = controle.value;
     const ucase = new RegExp("[A-Z]+");
     const lcase = new RegExp("[a-z]+");
     const num = new RegExp("[0-9]+");
-    const esp = new RegExp("(?=.*[}{,.^?~=+\\-_\\/*\\-+.\\|])");
-    const a = ucase.test(control.value);
-    console.log("Senha: " + control.value + " - Tem maiusculo: " + a);
-    const b = lcase.test(control.value);
-    console.log("Senha: " + control.value + " - Tem minusculo: " + b);
-    const c = num.test(control.value);
-    console.log("Senha: " + control.value + " - Tem número: " + c);
-    const d = esp.test(control.value);
-    console.log("Senha: " + control.value + " - Tem esp: " + d);
-    const forbidden = ucase.test(control.value) || lcase.test(control.value) || num.test(control.value);
-    console.log("Senha: " + control.value + " - válido: " + forbidden);
-    return forbidden ?
-        { 'password':
-          { value: control.value }
-    } : null;
+    const esp = new RegExp("(?=.*[%@!#¨&}{,.^?~=+\\'\\´\\`\\<\\>\\=\\(\\)\\[\\]\\-_\\/*\\-+.\\|])");
+
+    const ucaseItsValid = ucase.test(password);
+    console.log("Senha: " + password + " - Tem maiusculo: " + ucaseItsValid);
+    const lcaseItsValid = lcase.test(password);
+    console.log("Senha: " + password + " - Tem minusculo: " + lcaseItsValid);
+    const numItsValid = num.test(password);
+    console.log("Senha: " + password + " - Tem número: " + numItsValid);
+    const espItsValid = esp.test(password);
+    console.log("Senha: " + password + " - Tem esp: " + espItsValid);
+
+    const itsValid = (ucaseItsValid && lcaseItsValid && numItsValid && espItsValid);
+    console.log("Senha: " + password + " - válido: " + itsValid);
+    if (itsValid) {
+      return null;
+    }
+    return { passwordInvalido: true };
   }
 }
